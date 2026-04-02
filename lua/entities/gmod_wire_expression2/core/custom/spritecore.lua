4ecaa7461cca1ab97b370d997dc932797a0e3c87
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2026-04-02 16:35:42 -0400

    Refactor + add a few functions
    - Store created sprite entities on the chip data and decrement count on entity remove
    - Add `spriteRemoveAll()`
    - Add `spriteRemove(number index)`
    - Add `spriteSetColor(number index, vector color, number alpha)`
    - Add `spriteSetColor(number index, vector4 color)`
    - Cleaned up the overall code

--- lua/entities/gmod_wire_expression2/core/custom/spritecore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/spritecore.lua
@@ -1,21 +1,18 @@
 --[[
-	
+
 	Sprite Core E2 Extension
 	by NickMBR
-	
+
 	Release Date: 02/12/2016
 
 ]]--
 
 -- HEADER --
-E2Lib.RegisterExtension( "spritecore", false, "Create and Control Sprites with E2" )
+E2Lib.RegisterExtension("spritecore", false, "Create and Control Sprites with E2")
 
 -- CONVARS --
-local wire_spritecore_max = CreateConVar( "wire_spritecore_max", "100", FCVAR_ARCHIVE )
-local wire_spritecore_maxscale = CreateConVar( "wire_spritecore_maxscale", "16", FCVAR_ARCHIVE )
-
--- ARRAYS --
-local SpriteTable = {}
+local wire_spritecore_max = CreateConVar("wire_spritecore_max", "100", FCVAR_ARCHIVE)
+local wire_spritecore_maxscale = CreateConVar("wire_spritecore_maxscale", "16", FCVAR_ARCHIVE)
 
 local textures = {
 	"sprites/light_glow03.vmt",
@@ -58,85 +55,76 @@ local rendermodes = {
 }
 
 -- CHECK FUNCTIONS --
-local function ValidateSprite( self, index )
-	if SpriteTable[self.entity].count == wire_spritecore_max:GetInt() then
-		return false
+local function canSpawnSprite(self)
+	return self.data.sprite_count >= wire_spritecore_max:GetInt()
+end
+
+local function getSprite(self, index)
+	return self.data.sprites[index]
+end
+
+local function checkPath(path)
+	if not string.match(path, "%.vmt$") then
+		path = path .. ".vmt"
 	end
-	return true
+	return path
 end
 
-local function checkSprite( self, index )
-	if SpriteTable[self.entity].sprites[index] then
-		return false
-	end
-	return true
-end
-
-local function getSprite( self, index )
-	return SpriteTable[self.entity].sprites[index]
-end
-
-local function checkPath( path )
-
-	local pathfix
-	local str = string.find(path, ".vmt", 1)
-	
-	if str == nil then pathfix = path..".vmt" else pathfix = path end
-	
-	return pathfix
-end
+local color_white = Vector(255, 255, 255)
 
 -- SPRITE CREATOR --
-local function CreateSprite( self, Index, Path, Pos, Color, Alpha, Scale, Parent, RenderMode, Framerate)
-
+local function CreateSprite(self, index, path, pos, color, alpha, scale, parent, rendermode, framerate)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, Index ) then return end
+	if not canSpawnSprite(self) then return end
 
 	-- Sets a default sprite in case fields are nil
-	if not Pos then Pos = self.entity:GetPos() end
-	if not Path then Path = "sprites/glow02.vmt" end
-	if not RenderMode then RenderMode = 9 end
-	if not Color then Color = Vector( 255, 255, 255 ) end
-	if not Alpha then Alpha = 150 end
-	if not Framerate then Framerate = 10 end
-	if not Scale then Scale = 1 end
-	if not Parent then Parent = self.entity end
-	
+	if not path then path = "sprites/glow02.vmt" end
+	if not pos then pos = self.entity:GetPos() end
+	if not color then color = color_white end
+	if not alpha then alpha = 255 end
+	if not scale then scale = 1 end
+	if not parent or not E2Lib.isOwner(self, parent) then parent = self.entity end
+	if not rendermode then rendermode = 9 end
+	if not framerate then framerate = 10 end
+
 	-- Sets a maximum sprite scale
-	if Scale > wire_spritecore_maxscale:GetInt() then Scale = wire_spritecore_maxscale:GetInt() end
-	
+	if scale > wire_spritecore_maxscale:GetInt() then scale = wire_spritecore_maxscale:GetInt() end
+
 	-- Creates it
-	local SPR = ents.Create( "env_sprite" )
-	if not IsValid(SPR) then return end
-	
-	E2Lib.setPos(SPR, Pos)
-	SPR:SetMoveType(MOVETYPE_NONE)
-	
-	SPR:SetSaveValue( "rendermode", RenderMode )
-	SPR:SetSaveValue( "model", checkPath( Path ) )
-	SPR:SetSaveValue( "rendercolor", string.format( "%i %i %i", Color.x, Color.y, Color.z ) )
-	SPR:SetSaveValue( "framerate", Framerate )
-	SPR:SetSaveValue( "scale", Scale )
-	SPR:SetKeyValue( "renderamt", Alpha )
-	
-	-- updates the array
-	SpriteTable[self.entity].sprites[Index] = SPR
-	SpriteTable[self.entity].count = SpriteTable[self.entity].count + 1
-	
-	SPR:Spawn()
-	SPR:Activate()
-	SPR:SetParent( Parent )
-	
-	return SPR
+	local spr = ents.Create("env_sprite")
+	if not IsValid(spr) then return end
+
+	self.data.sprites[index] = spr
+	self.data.sprite_count = self.data.sprite_count + 1
+
+	spr:CallOnRemove("wire_expression2_spritecore_remove", function()
+		self.data.sprites[index] = nil
+		self.data.sprite_count = math.max(self.data.sprite_count - 1, 0)
+	end)
+
+	E2Lib.setPos(spr, pos)
+	spr:SetMoveType(MOVETYPE_NONE)
+
+	spr:SetSaveValue("model", checkPath(path))
+	spr:SetSaveValue("rendercolor", string.format("%i %i %i", color.x, color.y, color.z))
+	spr:SetKeyValue("renderamt", alpha)
+	spr:SetSaveValue("scale", scale)
+	spr:SetSaveValue("rendermode", rendermode)
+	spr:SetSaveValue("framerate", framerate)
+
+	spr:Spawn()
+	spr:Activate()
+	spr:SetParent(parent)
+
+	return spr
 end
 
 -- SPRITE REMOVER --
 local function spriteRemoveAll(self)
-	for k, v in pairs (SpriteTable[self.entity].sprites) do
+	local tbl = self.data.sprites
+	for _, v in pairs(self.data.sprites) do
 		if IsValid(v) then v:Remove() end
 	end
-	SpriteTable[self.entity].sprites = {}
-	SpriteTable[self.entity].count = 0
 end
 
 -- E2 FUNCTIONS --
@@ -146,197 +134,233 @@ end
 __e2setcost(20)
 
 -- SPRITE SPAWN FULL ARGS --
-e2function entity spriteSpawn( index, string path, vector pos, vector color, alpha, scale, entity parent, rendermode, framerate )
+e2function entity spriteSpawn(index, string path, vector pos, vector color, alpha, scale, entity parent, rendermode, framerate)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
-	
-	return CreateSprite( self, index, path, pos, color, alpha, scale, parent, rendermode, framerate )
+
+	return CreateSprite(self, index, path, pos, color, alpha, scale, parent, rendermode, framerate)
 end
 
 -- SPRITE SPAWN NO FRAMERATE --
-e2function entity spriteSpawn( index, string path, vector pos, vector color, alpha, scale, entity parent, rendermode )
+e2function entity spriteSpawn(index, string path, vector pos, vector color, alpha, scale, entity parent, rendermode)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
 
-	return CreateSprite( self, index, path, pos, color, alpha, scale, parent, rendermode )
+	return CreateSprite(self, index, path, pos, color, alpha, scale, parent, rendermode)
 end
 
 -- SPRITE SPAWN NO RENDERMODE --
-e2function entity spriteSpawn( index, string path, vector pos, vector color, alpha, scale, entity parent )
+e2function entity spriteSpawn(index, string path, vector pos, vector color, alpha, scale, entity parent)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
-	
-	return CreateSprite( self, index, path, pos, color, alpha, scale, parent )
+
+	return CreateSprite(self, index, path, pos, color, alpha, scale, parent)
 end
 
 -- SPRITE SPAWN NO PARENT --
-e2function entity spriteSpawn( index, string path, vector pos, vector color, alpha, scale )
+e2function entity spriteSpawn(index, string path, vector pos, vector color, alpha, scale)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
-	
-	return CreateSprite( self, index, path, pos, color, alpha, scale )
+
+	return CreateSprite(self, index, path, pos, color, alpha, scale)
 end
 
 -- SPRITE SPAWN NO SCALE --
-e2function entity spriteSpawn( index, string path, vector pos, vector color, alpha )
+e2function entity spriteSpawn(index, string path, vector pos, vector color, alpha)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
-	
-	return CreateSprite( self, index, path, pos, color, alpha )
+
+	return CreateSprite(self, index, path, pos, color, alpha)
 end
 
 -- SPRITE SPAWN NO ALPHA --
-e2function entity spriteSpawn( index, string path, vector pos, vector color )
+e2function entity spriteSpawn(index, string path, vector pos, vector color)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
-	
-	return CreateSprite( self, index, path, pos, color )
+
+	return CreateSprite(self, index, path, pos, color)
 end
 
 -- SPRITE SPAWN NO COLOR --
-e2function entity spriteSpawn( index, string path, vector pos )
+e2function entity spriteSpawn(index, string path, vector pos)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
 	color = Vector(color[1], color[2], color[3])
-	
-	return CreateSprite( self, index, path, pos )
+
+	return CreateSprite(self, index, path, pos)
 end
 
 -- SPRITE SPAWN NO POS --
-e2function entity spriteSpawn( index, string path )
+e2function entity spriteSpawn(index, string path)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
-	
-	return CreateSprite( self, index, path )
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
+	return CreateSprite(self, index, path)
 end
 
 -- SPRITE SPAWN ONLY INDEX --
-e2function entity spriteSpawn( index )
+e2function entity spriteSpawn(index)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	if not checkSprite( self, index ) then return end
+	if not canSpawnSprite(self) then return end
+	if getSprite(self, index) then return end
+
+	return CreateSprite(self, index)
+end
+
+e2function entity spriteRemoveAll()
+	if not self.player:IsValid() or not self.player:IsPlayer() then return end
 	
-	return CreateSprite( self, index)
+	spriteRemoveAll(self)
 end
 
 __e2setcost(10)
 
--- SPRITE TOGGLE --
-e2function void spriteEnable( index, value )
+-- SPRITE REMOVE --
+e2function entity spriteRemove(index)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
 	
 	local spr = getSprite(self, index)
-	if IsValid( spr ) then spr:SetNoDraw( value ~= 1 ) end
+	if IsValid(spr) then spr:Remove() end
+end
+
+-- SPRITE TOGGLE --
+e2function void spriteEnable(index, value)
+	if not self.player:IsValid() or not self.player:IsPlayer() then return end
+	if not canSpawnSprite(self) then return end
+
+	local spr = getSprite(self, index)
+	if IsValid(spr) then spr:SetNoDraw(value ~= 1) end
 end
 
 -- SPRITE SET POS --
-e2function void spriteSetPos( index, vector pos )
+e2function void spriteSetPos(index, vector pos)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+
 	pos = Vector(pos[1], pos[2], pos[3])
-	
+
 	local spr = getSprite(self, index)
-	if IsValid( spr ) then spr:SetPos( pos ) end
+	if IsValid(spr) then E2Lib.setPos(pos) end
 end
 
 -- SPRITE SET COLOR --
-e2function void spriteSetColor( index, vector color )
+e2function void spriteSetColor(index, vector color)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
-	color = Vector(color[1], color[2], color[3])
-	
+	if not canSpawnSprite(self) then return end
+
 	local spr = getSprite(self, index)
-	if IsValid( spr ) then spr:SetKeyValue( "rendercolor", string.format( "%i %i %i", color.x, color.y, color.z ) ) end
+	if IsValid(spr) then spr:SetKeyValue("rendercolor", string.format("%i %i %i", color[1], color[2], color[3])) end
+end
+
+-- SPRITE SET COLOR WITH ALPHA --
+e2function void spriteSetColor(index, vector color, alpha)
+	if not self.player:IsValid() or not self.player:IsPlayer() then return end
+	if not canSpawnSprite(self) then return end
+
+	local spr = getSprite(self, index)
+	if IsValid(spr) then
+		spr:SetKeyValue("rendercolor", string.format("%i %i %i", color[1], color[2], color[3]))
+		spr:SetKeyValue("renderamt", alpha)
+	end
+end
+
+-- SPRITE SET COLOR WITH VEC4 --
+e2function void spriteSetColor(index, vector4 color)
+	if not self.player:IsValid() or not self.player:IsPlayer() then return end
+	if not canSpawnSprite(self) then return end
+
+	local spr = getSprite(self, index)
+	if IsValid(spr) then
+		spr:SetKeyValue("rendercolor", string.format("%i %i %i", color[1], color[2], color[3]))
+		spr:SetKeyValue("renderamt", color[4])
+	end
 end
 
 -- SPRITE SET ALPHA --
-e2function void spriteSetAlpha( index, alpha )
+e2function void spriteSetAlpha(index, alpha)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+
 	local spr = getSprite(self, index)
-	if IsValid( spr ) then spr:SetKeyValue( "renderamt", alpha ) end
+	if IsValid(spr) then spr:SetKeyValue("renderamt", alpha) end
 end
 
 -- SPRITE SET SCALE --
-e2function void spriteSetScale( index, scale )
+e2function void spriteSetScale(index, scale)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+
 	local spr = getSprite(self, index)
-	
+
 	if scale > wire_spritecore_maxscale:GetInt() then scale = wire_spritecore_maxscale:GetInt() end
-	if IsValid( spr ) then spr:SetKeyValue( "scale", scale ) end
+	if IsValid(spr) then spr:SetKeyValue("scale", scale) end
 end
 
 -- SPRITE SET PARENT --
-e2function void spriteSetParent( index, entity parent )
+e2function void spriteSetParent(index, entity parent)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+
 	local spr = getSprite(self, index)
-	if IsValid( spr ) and IsValid(parent) then spr:SetParent( parent ) end
+	if IsValid(spr) and IsValid(parent) then spr:SetParent(parent) end
 end
 
 -- SPRITE SET RENDERMODE --
-e2function void spriteSetRendermode( index, rendermode )
+e2function void spriteSetRendermode(index, rendermode)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+
 	local spr = getSprite(self, index)
-	if IsValid( spr ) then spr:SetKeyValue( "rendermode", rendermode ) end
+	if IsValid(spr) then spr:SetKeyValue("rendermode", rendermode) end
 end
 
 -- SPRITE SET FRAMERATE --
-e2function void spriteSetFramerate( index, framerate )
+e2function void spriteSetFramerate(index, framerate)
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
-	if not ValidateSprite( self, index ) then return end
-	
+	if not canSpawnSprite(self) then return end
+
 	local spr = getSprite(self, index)
-	if IsValid( spr ) then spr:SetKeyValue( "framerate", framerate ) end
+	if IsValid(spr) then spr:SetKeyValue("framerate", framerate) end
 end
 
 -- PRINT AVALIABLE SPRITE LIST --
 e2function void spriteList()
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
 	if not textures then return end
-	
+
 	self.player:ChatPrint("Sprite List:")
 	for k, v in pairs (textures) do
 		self.player:ChatPrint(v)
 	end
-	
+
 	self.player:ChatPrint("[SpriteCore] Open the chat to see the texture list above.")
 end
 
@@ -344,21 +368,19 @@ end
 e2function void spriteRList()
 	if not self.player:IsValid() or not self.player:IsPlayer() then return end
 	if not textures then return end
-	
+
 	self.player:ChatPrint("Render Modes:")
-	for k, v in pairs (rendermodes) do
+	for k, v in pairs(rendermodes) do
 		self.player:ChatPrint(v)
 	end
-	
+
 	self.player:ChatPrint("[SpriteCore] Open the chat to see the render modes list above.")
 end
 
 -- CALLBACKS --
 registerCallback("construct", function(self)
-	SpriteTable[self.entity] = {
-		sprites = {},
-		count = 0
-	}
+	self.data.sprites = {}
+	self.data.sprite_count = 0
 end)
 
 registerCallback("destruct", function(self)
